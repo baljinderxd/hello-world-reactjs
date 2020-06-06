335d1d84e4abdb89fc3680c11f2256795636b5f1
baljinderxd <57032785+baljinderxd@users.noreply.github.com>
2020-06-06 12:57:14 +0530

    ...

--- src/components/aaa.js
+++ src/components/aaa.js
@@ -1,28 +1,7 @@
-var result = new Array, flag = -1, answer = ''
-var PdfReader = require("pdfreader").PdfReader
-new PdfReader().parseFileItems(__dirname + '/sem4.pdf',
-            function (err, item) {
-                
-                if (err) {
-                    console.log('inside pdf reader err')
-                    console.log(err)
-                    res.end(err)
-                }
-                if (item && item.text)
-                    result.push(item.text)
-                if (!item && result.length !== 0) {
-                    for (let i = 0; i < result.length; i++) {
-                        if (result[i] === '00413202717') {
-                            flag = i
-                            console.log('File readed')
-                        }
-                    }
-                    if (flag === -1)
-                        answer = 'Sorry!!No result Found'
-                    else {
-                        for (let i = flag; i < flag + 20; i++)
-                            answer += result[i]
-                    }
-                   console.log(answer)
-                }
-            })
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [...arr1, 6];
+const func = (...rest) => {
+  console.log(rest.length);
+}
+func(...arr1);
+func(...arr2);
